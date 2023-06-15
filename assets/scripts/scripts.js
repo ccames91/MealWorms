ffc770dba026624810c973d562270ad7a1c4852e
Chandrasekar Mohan <chandrasekar.mohan@gmail.com>
2023-06-15 15:06:45 -0700

    Added history  button logic

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -2,6 +2,8 @@ var userFormEl = document.querySelector("#user-form");
 var receipeInputEl = document.querySelector("#RecipeSearch");
 var receipeContainerEl = document.querySelector("#receipe-container");
 
+var historyContainerEl = document.querySelector("#history-buttons");
+
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -13,9 +15,31 @@ var formSubmitHandler = function (event) {
         return
     }
 
+   
+
+
+    if (!recipeSearch[searchText]) {
+        createHistoryButton(searchText, true);
+        } else {
+        for (const elIndex in historyContainerEl.children) {
+            let buttonEl = historyContainerEl.children[elIndex];
+
+            if ( buttonEl.classList && buttonEl.dataset.recipe === searchText) {
+                buttonEl.classList.toggle("active");
+            }
+        }
+    }
+
+    recipeSearch[searchText] = searchText;
+    localStorage.setItem("recipeSearchData", JSON.stringify(recipeSearch));
+
+    getRecipeList(searchText);
+}
+
+var getRecipeList = function(searchText) {
     fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + searchText)
-        .then(res => {
-         return res.json()
+    .then(res => {
+        return res.json()
     })
     .then(data => {
         receipeContainerEl.innerHTML = ''
@@ -57,12 +81,53 @@ var createRecipeCard = function (currentDayData) {
 
     var cardDetails = document.createElement("p");
     cardDetails.textContent = currentDayData.strInstructions
-    
+
     cardBodyEl.appendChild(cardDetails);
     cardEl.appendChild(cardBodyEl);
 
     receipeContainerEl.appendChild(cardEl);
+};
+
+//Function to generate history button for each previous searched recipes
+var createHistoryButton = function (recipeSearch, isActive) {
+    let buttonEl = document.createElement("button");
+    buttonEl.classList = "btn";
+    buttonEl.setAttribute("data-recipe", recipeSearch);
+    buttonEl.innerText = recipeSearch;
+  
+    buttonEl.addEventListener("click", buttonClickHandler);
+  
+    if (isActive) {
+      buttonEl.classList.toggle("active");
+    }
+  
+    historyContainerEl.appendChild(buttonEl);
   };
+  
+  var buttonClickHandler = function (event) {
+    //disable active button
+    for (const elIndex in historyContainerEl.children) {
+      let buttonEl = historyContainerEl.children[elIndex];
+      if (buttonEl.classList && buttonEl.classList.contains("active")) {
+        buttonEl.classList.toggle("active");
+      }
+    }
+    getRecipeList(event.target.dataset.recipe);
+};
+  
+  //Look for localstorage for any previous search history and create action button for those recipes
+  let recipeSearch = JSON.parse(localStorage.getItem("recipeSearchData"));
+  historyContainerEl.innerHTML = "";
+  
+  if (recipeSearch) {
+    for (const key in Object.keys(recipeSearch)) {
+      let searchString = Object.keys(recipeSearch)[key];
+      console.log(searchString);
+      createHistoryButton(searchString);
+    }
+  } else {
+    recipeSearch = {};
+  }
 
 //form submit event listener.
 userFormEl.addEventListener("submit", formSubmitHandler);
