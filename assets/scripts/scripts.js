2867fccb90eb2b988cf2da5adc41eafb3e91dfb4
Chandrasekar Mohan <chandrasekar.mohan@gmail.com>
2023-06-15 11:57:34 -0700

    Search display changes

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,14 +1,23 @@
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Pasta')
-.then(res => {
-    return res.json()
-})
-.then(data => {
-    const markup = '<p>s=Pasta</p>'
-
-    document.querySelector('#recipe').innerHTML=markup
-    console.log (data)
-});
+var userFormEl = document.querySelector("#user-form");
 
 
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Pasta')
+        .then(res => {
+         return res.json()
+    })
+    .then(data => {
+        const markup = '<p>s=Pasta</p>'
+
+        document.querySelector('#recipe').innerHTML=markup
+        console.log (data)
+    });
+}
 
 
+
+
+
+//form submit event listener.
+userFormEl.addEventListener("submit", formSubmitHandler);
